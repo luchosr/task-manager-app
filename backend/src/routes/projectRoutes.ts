48035d629156698c221c743b36c0e9e0ec5fab74
Luciano Ramello <luchosr@gmail.com>
2024-11-14 13:02:57 +0100

    feat: updates routes with get and put methods

--- backend/src/routes/projectRoutes.ts
+++ backend/src/routes/projectRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { body } from 'express-validator';
+import { body, param } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware/validation';
 
@@ -13,5 +13,22 @@ router.post(
   ProjectController.createProject
 );
 router.get('/', ProjectController.getAllProjects);
+
+router.get(
+  '/:id',
+  param('id').isMongoId().withMessage(' ID is not valid'),
+  handleInputErrors,
+  ProjectController.getProjectById
+);
+
+router.put(
+  '/:id',
+  param('id').isMongoId().withMessage(' ID is not valid'),
+  body('projectName').not().isEmpty().withMessage('Project name is required'),
+  body('clientName').not().isEmpty().withMessage('Client name is required'),
+  body('description').not().isEmpty().withMessage('Description is required'),
+  handleInputErrors,
+  ProjectController.updateProject
+);
 
 export default router;
