83b282621c58766035f3fe852932f33661cd1106
Luciano Ramello <luchosr@gmail.com>
2024-11-14 11:19:25 +0100

    feat: adds validation to post route

--- backend/src/routes/projectRoutes.ts
+++ backend/src/routes/projectRoutes.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { ProjectController } from '../controllers/ProjectController';
+import { handleInputErrors } from '../middleware/validation';
 
 const router = Router();
-router.post('/', ProjectController.createProject);
+router.post(
+  '/',
+  body('projectName').not().isEmpty().withMessage('Project name is required'),
+  body('clientName').not().isEmpty().withMessage('Client name is required'),
+  body('description').not().isEmpty().withMessage('Description is required'),
+  handleInputErrors,
+  ProjectController.createProject
+);
 router.get('/', ProjectController.getAllProjects);
 
 export default router;
