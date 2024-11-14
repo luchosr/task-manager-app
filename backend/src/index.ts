020f3179616735ed2ccf42a6148c13e80fcfebae
Luciano Ramello <luchosr@gmail.com>
2024-11-14 09:26:41 +0100

    feat: implements colors on server connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,5 +4,5 @@ import server from './server';
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(colors.bgCyan(`Server running on port ${port}`));
+  console.log(colors.cyan.bold(`Server running on port ${port}`));
 });
