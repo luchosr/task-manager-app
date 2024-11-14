8f790b34cc33ea6655f4ea7b2575020b2e1fdcd6
Luciano Ramello <luchosr@gmail.com>
2024-11-14 13:03:28 +0100

    feat: implements getprojectbyID and update project controllers

--- backend/src/controllers/ProjectController.ts
+++ backend/src/controllers/ProjectController.ts
@@ -13,6 +13,48 @@ export class ProjectController {
   };
 
   static getAllProjects = async (req: Request, res: Response) => {
-    res.send('all projects');
+    try {
+      const projects = await Project.find({});
+      res.json(projects);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static getProjectById = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findById(id);
+
+      if (!project) {
+        const error = new Error('Cannot find the project');
+        res.status(400).json({ error: error.message });
+        return;
+      }
+
+      res.json(project); // Enviar la respuesta pero no devolverla
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static updateProject = async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findByIdAndUpdate(id, req.body);
+
+      if (!project) {
+        const error = new Error('Cannot find the project');
+        res.status(400).json({ error: error.message });
+        return;
+      }
+      await project.save();
+      res.send('Project Updated');
+    } catch (error) {
+      console.log(error);
+    }
   };
 }
