96628dff2002027ebcaa5dccb513652b2160053a
Luciano Ramello <luchosr@gmail.com>
2024-11-15 10:44:14 +0100

    feat: implements promise all settled method

--- backend/src/controllers/TaskController.ts
+++ backend/src/controllers/TaskController.ts
@@ -8,8 +8,7 @@ export class TaskController {
       const task = new Task(req.body);
       task.project = req.project.id;
       req.project.tasks.push(task.id);
-      await task.save();
-      await req.project.save();
+      await Promise.allSettled([task.save(), req.project.save()]);
       res.send('Task Created successfully');
     } catch (error) {
       console.log(error);
