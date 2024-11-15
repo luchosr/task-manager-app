e160b356a93f4cedeb963be689abdd2edd1b03ef
Luciano Ramello <luchosr@gmail.com>
2024-11-15 10:11:07 +0100

    feat: improves task controller

--- backend/src/controllers/TaskController.ts
+++ backend/src/controllers/TaskController.ts
@@ -4,20 +4,12 @@ import Task from '../models/Task';
 
 export class TaskController {
   static createTask = async (req: Request, res: Response) => {
-    const { projectId } = req.params;
-
-    const project = await Project.findById(projectId);
-    if (!project) {
-      const error = new Error('Cannot find the project');
-      res.status(404).json({ error: error.message });
-      return;
-    }
     try {
       const task = new Task(req.body);
-      task.project = project.id;
-      project.tasks.push(task.id);
+      task.project = req.project.id;
+      req.project.tasks.push(task.id);
       await task.save();
-      await project.save();
+      await req.project.save();
       res.send('Task Created successfully');
     } catch (error) {
       console.log(error);
