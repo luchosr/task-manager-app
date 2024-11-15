6f0b55e629d0f2110b9ec00438b40c9d94babd01
Luciano Ramello <luchosr@gmail.com>
2024-11-15 11:23:24 +0100

    feat: improved error handling on task controller

--- backend/src/controllers/TaskController.ts
+++ backend/src/controllers/TaskController.ts
@@ -11,7 +11,18 @@ export class TaskController {
       await Promise.allSettled([task.save(), req.project.save()]);
       res.send('Task Created successfully');
     } catch (error) {
-      console.log(error);
+      res.status(500).json({ error: 'Ups! Something went wrong' });
+    }
+  };
+
+  static getProjectTasks = async (req: Request, res: Response) => {
+    try {
+      const tasks = await Task.find({ project: req.project.id }).populate(
+        'project'
+      );
+      res.json(tasks);
+    } catch (error) {
+      res.status(500).json({ error: 'Ups! Something went wrong' });
     }
   };
 }
