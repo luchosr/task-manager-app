c0c6b42bff06d3d097ae42df12271a6f546e0385
Luciano Ramello <luchosr@gmail.com>
2024-11-16 12:48:50 +0100

    feat: creates tasks middleware

---
+++ backend/src/middleware/task.ts
@@ -0,0 +1,31 @@
+import type { Request, Response, NextFunction } from 'express';
+import Task, { ITask } from '../models/Task';
+
+declare global {
+  namespace Express {
+    interface Request {
+      task: ITask;
+    }
+  }
+}
+export async function taskExists(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { taskId } = req.params;
+
+    const task = await Task.findById(taskId);
+    if (!task) {
+      const error = new Error('Cannot find the task');
+      res.status(404).json({ error: error.message });
+      return;
+    }
+
+    req.task = task;
+    next();
+  } catch (error) {
+    res.status(500).json({ error: 'Ups! Something went wrong' });
+  }
+}
