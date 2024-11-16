cea31f8952a979a352b21b8952358337d947959d
Luciano Ramello <luchosr@gmail.com>
2024-11-16 12:09:38 +0100

    feat: adds delete and update tasks controller

--- backend/src/controllers/TaskController.ts
+++ backend/src/controllers/TaskController.ts
@@ -50,7 +50,7 @@ export class TaskController {
   static updateTask = async (req: Request, res: Response) => {
     try {
       const { taskId } = req.params;
-      const task = await Task.findByIdAndUpdate(taskId, req.body);
+      const task = await Task.findById(taskId);
       if (!task) {
         const error = new Error('Cannot find the Task');
         res.status(404).json({ error: error.message });
@@ -62,7 +62,58 @@ export class TaskController {
         res.status(404).json({ error: error.message });
         return;
       }
+
+      task.name = req.body.name;
+      task.description = req.body.description;
+      await task.save();
       res.send('Task updated successfully');
+    } catch (error) {
+      res.status(500).json({ error: 'Ups! Something went wrong' });
+    }
+  };
+
+  static deleteTask = async (req: Request, res: Response) => {
+    try {
+      const { taskId } = req.params;
+      const task = await Task.findByIdAndUpdate(taskId);
+      if (!task) {
+        const error = new Error('Cannot find the Task');
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      if (task.project.toString() !== req.project.id) {
+        const error = new Error('Task does not belong to this project');
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      req.project.tasks = req.project.tasks.filter(
+        (task) => task.toString() !== taskId
+      );
+
+      await Promise.allSettled([task.deleteOne(), req.project.save()]);
+
+      res.send('Task Removed successfully');
+    } catch (error) {
+      res.status(500).json({ error: 'Ups! Something went wrong' });
+    }
+  };
+
+  static updateTaskStatus = async (req: Request, res: Response) => {
+    try {
+      const { taskId } = req.params;
+      const task = await Task.findById(taskId);
+      if (!task) {
+        const error = new Error('Cannot find the Task');
+        res.status(404).json({ error: error.message });
+        return;
+      }
+
+      const { status } = req.body;
+      task.status = status;
+      await task.save();
+      res.send('Task status updated successfully');
     } catch (error) {
       res.status(500).json({ error: 'Ups! Something went wrong' });
     }
