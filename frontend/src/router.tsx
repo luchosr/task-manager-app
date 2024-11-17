54b56ec8203e3e2bca5bc50e12698a88788fdbf3
Luciano Ramello <luchosr@gmail.com>
2024-11-17 17:33:13 +0100

    feat: adds route for projects creation

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from '@/layouts/AppLayout';
 import DashboardView from '@/views/DashboardView';
+import CreateProjectView from './views/CreateProjectView';
 
 export default function Router() {
   return (
@@ -8,6 +9,12 @@ export default function Router() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<DashboardView />} index />
+          <Route
+            path="/projects/create"
+            element={<CreateProjectView />}
+            index
+          />
+
           <Route />
           <Route />
           <Route />
