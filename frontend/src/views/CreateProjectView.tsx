dd0fb83cd7a30caaae2b7f892411c3fb492c3609
Luciano Ramello <luchosr@gmail.com>
2024-11-17 17:33:35 +0100

    feat: creates create project view component

---
+++ frontend/src/views/CreateProjectView.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+export default function CreateProjectView() {
+  return (
+    <>
+      <h1 className="text-5xl font-black">Crear Proyecto</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">
+        Llena el siguiente formulario para crear un nuevo proyecto
+      </p>
+      <nav className="my-5">
+        <Link
+          to={'/'}
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+        >
+          Volver a Proyectos
+        </Link>
+      </nav>
+    </>
+  );
+}
