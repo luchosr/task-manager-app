83c3b4e6bfcd4d05715ddbfe79c32630c78e98fe
Luciano Ramello <luchosr@gmail.com>
2024-11-18 17:54:33 +0100

    feat: updates data types

--- frontend/src/views/CreateProjectView.tsx
+++ frontend/src/views/CreateProjectView.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import ProjectForm from '@/components/projects/ProjectForm';
+import { ProjectFormData } from 'types';
 
 export default function CreateProjectView() {
-  const initialValues = {
+  const initialValues: ProjectFormData = {
     projectName: '',
     clientName: '',
     description: '',
@@ -14,7 +15,7 @@ export default function CreateProjectView() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const handleForm = (data) => {
+  const handleForm = (data: ProjectFormData) => {
     console.log(data);
   };
 
