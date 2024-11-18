2e03523a40ff42338cd46df1b337a69f877304ae
Luciano Ramello <luchosr@gmail.com>
2024-11-18 17:23:59 +0100

    feat: adds useform hook

--- frontend/src/views/CreateProjectView.tsx
+++ frontend/src/views/CreateProjectView.tsx
@@ -1,8 +1,25 @@
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import ProjectForm from '@/components/projects/ProjectForm';
 
 export default function CreateProjectView() {
+  const initialValues = {
+    projectName: '',
+    clientName: '',
+    description: '',
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const handleForm = (data) => {
+    console.log(data);
+  };
+
   return (
-    <>
+    <div className="max-w-3xl mx-auto">
       <h1 className="text-5xl font-black">Crear Proyecto</h1>
       <p className="text-2xl font-light text-gray-500 mt-5">
         Llena el siguiente formulario para crear un nuevo proyecto
@@ -15,6 +32,19 @@ export default function CreateProjectView() {
           Volver a Proyectos
         </Link>
       </nav>
-    </>
+      <form
+        action=""
+        className="mt-10 bg-white shadow-lg rounded-lg p-10"
+        onSubmit={handleSubmit(handleForm)}
+        noValidate
+      >
+        <ProjectForm register={register} errors={errors} />
+        <input
+          type="submit"
+          value="Crear Proyecto"
+          className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors text-center"
+        />
+      </form>
+    </div>
   );
 }
