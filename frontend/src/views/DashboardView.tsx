391ccaecc277f5dc82fda40e6fd6f4b748982509
Luciano Ramello <luchosr@gmail.com>
2024-11-19 17:42:06 +0100

    feat: implements useQuery on DashboardView

--- frontend/src/views/DashboardView.tsx
+++ frontend/src/views/DashboardView.tsx
@@ -1,6 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getProjects } from '@/api/ProjectAPI';
 
 export default function DashboardView() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['projects'],
+    queryFn: getProjects,
+  });
+
+  if (isLoading) {
+    return <h3 className="text-2xl font-black">Cargando...</h3>;
+  }
+  console.log(isLoading);
+  console.log(data);
+
   return (
     <>
       <h1 className="text-5xl font-black">Mis Proyectos</h1>
