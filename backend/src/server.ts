b63f611914b2cd7024d0ea88249220915c1c30ff
Luciano Ramello <luchosr@gmail.com>
2024-11-19 11:22:59 +0100

    feat: implements cors on server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
-
 import projectRoutes from './routes/projectRoutes';
 
 dotenv.config();
@@ -9,6 +10,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors(corsConfig));
 
 app.use(express.json());
 
