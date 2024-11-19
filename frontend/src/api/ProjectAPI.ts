2971c656dcb97f224eb03dd8fc9c9fcfe466a376
Luciano Ramello <luchosr@gmail.com>
2024-11-19 18:20:18 +0100

    feat: improves get projects api function

--- frontend/src/api/ProjectAPI.ts
+++ frontend/src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import api from '@/lib/axios';
-import { ProjectFormData } from '../types';
+import { dashBoardProjectSchema, ProjectFormData } from '../types';
 import { isAxiosError } from 'axios';
 
 export async function createProject(formData: ProjectFormData) {
@@ -16,7 +16,10 @@ export async function createProject(formData: ProjectFormData) {
 export async function getProjects() {
   try {
     const { data } = await api('/projects');
-    return data;
+    const response = dashBoardProjectSchema.safeParse(data);
+    if (response.success) {
+      return response.data;
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);
