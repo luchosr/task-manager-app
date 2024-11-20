b9adf617dab4e197e1464179fc47d2908cd54250
Luciano Ramello <luchosr@gmail.com>
2024-11-20 20:32:14 +0100

    feat: implements morgan on server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import morgan from 'morgan';
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
@@ -12,7 +13,7 @@ connectDB();
 const app = express();
 
 app.use(cors(corsConfig));
-
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api/projects', projectRoutes);
