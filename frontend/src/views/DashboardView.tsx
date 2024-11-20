4cac258402e2845a0f81dbea6161a87c1cc2519c
Luciano Ramello <luchosr@gmail.com>
2024-11-20 20:34:21 +0100

    feat: implements delete  mutation on dashboard view

--- frontend/src/views/DashboardView.tsx
+++ frontend/src/views/DashboardView.tsx
@@ -8,14 +8,24 @@ import {
   Transition,
 } from '@headlessui/react';
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
-
-import { useQuery } from '@tanstack/react-query';
-import { getProjects } from '@/api/ProjectAPI';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteProject, getProjects } from '@/api/ProjectAPI';
+import { toast } from 'react-toastify';
 
 export default function DashboardView() {
   const { data, isLoading } = useQuery({
     queryKey: ['projects'],
     queryFn: getProjects,
+  });
+
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: deleteProject,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ['projects'] });
+    },
   });
 
   if (isLoading) {
@@ -101,7 +111,7 @@ export default function DashboardView() {
                           <button
                             type="button"
                             className="block px-3 py-1 text-sm leading-6 text-red-500"
-                            onClick={() => {}}
+                            onClick={() => mutate(project._id)}
                           >
                             Eliminar Proyecto
                           </button>
