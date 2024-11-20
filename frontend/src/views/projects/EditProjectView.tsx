de4559b287b1b61cce1ec0adfa693977d005d67f
Luciano Ramello <luchosr@gmail.com>
2024-11-20 12:44:49 +0100

    feat: improves edit project view component

--- frontend/src/views/projects/EditProjectView.tsx
+++ frontend/src/views/projects/EditProjectView.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import { getProjectById } from '@/api/ProjectAPI';
+import { useQuery } from '@tanstack/react-query';
+import { Navigate, useParams } from 'react-router-dom';
+import EditProjectForm from './EditProjectForm';
 
 export default function EditProjectView() {
-  return <div>EditProjectView</div>;
+  const params = useParams();
+
+  const projectId = params.projectId!;
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['editProject', projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false,
+  });
+
+  if (isLoading) return <p>Cargando...</p>;
+
+  if (isError) return <Navigate to={'/404'} />;
+
+  if (data) return <EditProjectForm data={data} />;
 }
