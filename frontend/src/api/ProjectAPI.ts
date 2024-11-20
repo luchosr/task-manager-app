8cfdd028852d2dc80c2ad70782488562709aa023
Luciano Ramello <luchosr@gmail.com>
2024-11-20 20:33:41 +0100

    feat: upgrades Project API's methods

--- frontend/src/api/ProjectAPI.ts
+++ frontend/src/api/ProjectAPI.ts
@@ -37,3 +37,29 @@ export async function getProjectById(id: Project['_id']) {
     }
   }
 }
+
+type ProjectAPIType = {
+  projectId: Project['_id'];
+  formData: ProjectFormData;
+};
+export async function updateProject({ formData, projectId }: ProjectAPIType) {
+  try {
+    const { data } = await api.put<string>(`/projects/${projectId}`, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function deleteProject(id: Project['_id']) {
+  try {
+    const { data } = await api.delete(`/projects/${id}`);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
