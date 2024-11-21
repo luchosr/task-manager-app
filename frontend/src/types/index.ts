2826976d4f2d047722072976ebf50fc0368a70c6
Luciano Ramello <luchosr@gmail.com>
2024-11-21 23:52:36 +0100

    feat: implements taskStatus schema

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,5 +1,30 @@
 import { z } from 'zod';
 
+type TaskStatus =
+  | 'pending'
+  | 'on hold'
+  | 'in progress'
+  | 'under review'
+  | 'completed';
+
+export const taskStatusSchema = z.enum([
+  'pending',
+  'on hold',
+  'in progress',
+  'under review',
+  'completed',
+]);
+
+export type Task = z.infer<typeof taskSchema>;
+
+export type TaskFormData = Pick<Task, 'name' | 'description'>;
+export const taskSchema = z.object({
+  _id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  project: z.string(),
+  status: z.string(),
+});
 /** Projects */
 
 export const projectSchema = z.object({
