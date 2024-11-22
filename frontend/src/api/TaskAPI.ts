47a347c7d4e12d7ec09a0515af01e4ffe5363ab3
Luciano Ramello <luchosr@gmail.com>
2024-11-22 20:12:04 +0100

    feat: adds create task method on task API

---
+++ frontend/src/api/TaskAPI.ts
@@ -0,0 +1,22 @@
+import { AxiosError, isAxiosError } from 'axios';
+import api from '@/lib/axios';
+import { Project, TaskFormData } from '../types';
+
+type TaskAPIType = {
+  formData: TaskFormData;
+  projectId: Project['_id'];
+};
+export async function createTask({
+  formData,
+  projectId,
+}: Pick<TaskAPIType, 'formData' | 'projectId'>) {
+  try {
+    const url = `/projects/${projectId}/tasks`;
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
