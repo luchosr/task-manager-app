184ac426e7371b3990abc56bc62681771f68068e
Luciano Ramello <luchosr@gmail.com>
2024-11-25 11:13:06 +0100

    feat: improves  EditTaskData component

--- frontend/src/components/tasks/EditTaskData.tsx
+++ frontend/src/components/tasks/EditTaskData.tsx
@@ -1,11 +1,20 @@
-import { useLocation } from 'react-router-dom'
+import { getTaskById } from "@/api/TaskAPI";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation, useParams } from "react-router-dom";
+import EditTaskModal from "./EditTaskModal";
 
 export default function EditTaskData() {
+  const params = useParams();
+  const projectId = params.projectId!;
   const location = useLocation();
-  const queryParamas = new URLSearchParams(location.search);
-  const editTask = queryParamas.get('editTask');
-  console.log(editTask);
-  return (
-    <div>EditTaskData</div>
-  )
+  const queryParams = new URLSearchParams(location.search);
+  const taskId = queryParams.get("editTask")!;
+
+  const { data } = useQuery({
+    queryKey: ["task", taskId],
+    queryFn: () => getTaskById({ projectId, taskId }),
+    enabled: !!taskId,
+  });
+
+  if (data) return <EditTaskModal />;
 }
