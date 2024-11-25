a6240c86992111fb8b27646450debf2fa78f8e77
Luciano Ramello <luchosr@gmail.com>
2024-11-25 15:39:33 +0100

    feat: implements edit task modal

--- frontend/src/components/tasks/EditTaskData.tsx
+++ frontend/src/components/tasks/EditTaskData.tsx
@@ -1,20 +1,22 @@
-import { getTaskById } from "@/api/TaskAPI";
-import { useQuery } from "@tanstack/react-query";
-import { useLocation, useParams } from "react-router-dom";
-import EditTaskModal from "./EditTaskModal";
+import { getTaskById } from '@/api/TaskAPI';
+import { useQuery } from '@tanstack/react-query';
+import { Navigate, useLocation, useParams } from 'react-router-dom';
+import EditTaskModal from './EditTaskModal';
 
 export default function EditTaskData() {
   const params = useParams();
   const projectId = params.projectId!;
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const taskId = queryParams.get("editTask")!;
+  const taskId = queryParams.get('editTask')!;
 
-  const { data } = useQuery({
-    queryKey: ["task", taskId],
+  const { data, isError } = useQuery({
+    queryKey: ['task', taskId],
     queryFn: () => getTaskById({ projectId, taskId }),
     enabled: !!taskId,
   });
 
-  if (data) return <EditTaskModal />;
+  if (isError) return <Navigate to={'/404'} />;
+
+  if (data) return <EditTaskModal data={data} taskId={taskId} />;
 }
