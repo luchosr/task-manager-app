3ecee09ff7bae5281f90e1eb7c36191209061f23
Luciano Ramello <luchosr@gmail.com>
2024-11-25 15:38:53 +0100

    fix: improves Task API type

--- frontend/src/api/TaskAPI.ts
+++ frontend/src/api/TaskAPI.ts
@@ -1,16 +1,16 @@
-import { isAxiosError } from "axios";
-import api from "@/lib/axios";
-import { Project, Task, TaskFormData } from "../types";
+import { isAxiosError } from 'axios';
+import api from '@/lib/axios';
+import { Project, Task, TaskFormData } from '../types';
 
 type TaskAPIType = {
   formData: TaskFormData;
-  projectId: Project["_id"];
-  taskId: Task["_id"];
+  projectId: Project['_id'];
+  taskId: Task['_id'];
 };
 export async function createTask({
   formData,
   projectId,
-}: Pick<TaskAPIType, "formData" | "projectId">) {
+}: Pick<TaskAPIType, 'formData' | 'projectId'>) {
   try {
     const url = `/projects/${projectId}/tasks`;
     const { data } = await api.post<string>(url, formData);
@@ -25,10 +25,41 @@ export async function createTask({
 export async function getTaskById({
   projectId,
   taskId,
-}: Pick<TaskAPIType, "projectId" | "taskId">) {
+}: Pick<TaskAPIType, 'projectId' | 'taskId'>) {
   try {
     const url = `/projects/${projectId}/tasks/${taskId}`;
     const { data } = await api(url);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function updateTask({
+  projectId,
+  taskId,
+  formData,
+}: Pick<TaskAPIType, 'projectId' | 'taskId' | 'formData'>) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}`;
+    const { data } = await api.put<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function deleteTask({
+  projectId,
+  taskId,
+}: Pick<TaskAPIType, 'projectId' | 'taskId'>) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}`;
+    const { data } = await api.delete<string>(url);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
