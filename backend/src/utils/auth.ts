a4d40711190273fa574b71f6ba51708ae80a73e8
Luciano Ramello <luchosr@gmail.com>
2024-12-04 12:20:30 +0100

    feat: creates password checker

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -4,3 +4,10 @@ export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
 };
+
+export const checkPassword = async (
+  password: string,
+  storedHash: string
+): Promise<boolean> => {
+  return bcrypt.compare(password, storedHash);
+};
