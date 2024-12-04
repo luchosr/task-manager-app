a7916ea71a10c5a52c6db24b80d9b643c995a2f7
Luciano Ramello <luchosr@gmail.com>
2024-12-04 12:53:40 +0100

    feat: implements Auth Layout route

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -4,6 +4,8 @@ import DashboardView from '@/views/DashboardView';
 import CreateProjectView from './views/projects/CreateProjectView';
 import EditProjectView from './views/projects/EditProjectView';
 import ProjectDetailsView from './views/projects/ProjectDetailsView';
+import AuthLayout from './layouts/AuthLayout';
+import LoginView from './views/auth/LoginView';
 
 export default function Router() {
   return (
@@ -17,10 +19,9 @@ export default function Router() {
             path="/projects/:projectId/edit"
             element={<EditProjectView />}
           />
-          <Route />
-          <Route />
-          <Route />
-          <Route />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="/auth/login" element={<LoginView />} />
         </Route>
       </Routes>
     </BrowserRouter>
