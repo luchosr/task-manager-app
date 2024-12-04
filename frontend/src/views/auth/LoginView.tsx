db979ec6a171fb1a4fa83894cf9c407aa0b6df7d
Luciano Ramello <luchosr@gmail.com>
2024-12-04 16:36:15 +0100

    feat: improves Login View component

--- frontend/src/views/auth/LoginView.tsx
+++ frontend/src/views/auth/LoginView.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { UserLoginForm } from '@/types/index';
 import ErrorMessage from '@/components/ErrorMessage';
@@ -63,6 +64,14 @@ export default function LoginView() {
           className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
         />
       </form>
+      <nav className="mt-10 flex flex-col space-y-4">
+        <Link
+          to={'/auth/register'}
+          className="text-center text-gray-300 font-normal"
+        >
+          No tienes cuenta? Regístrate y crea una.
+        </Link>
+      </nav>
     </>
   );
 }
