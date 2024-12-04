38858aa28519ab12f3163025ac3ca8a2ac991dd3
Luciano Ramello <luchosr@gmail.com>
2024-12-04 17:06:53 +0100

    feat: implements register mutation in register view

--- frontend/src/views/auth/RegisterView.tsx
+++ frontend/src/views/auth/RegisterView.tsx
@@ -2,6 +2,9 @@ import { useForm } from 'react-hook-form';
 import { UserRegistrationForm } from '@/types/index';
 import ErrorMessage from '@/components/ErrorMessage';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { createAccount } from '@/api/AuthAPI';
+import { toast } from 'react-toastify';
 
 export default function RegisterView() {
   const initialValues: UserRegistrationForm = {
@@ -19,9 +22,18 @@ export default function RegisterView() {
     formState: { errors },
   } = useForm<UserRegistrationForm>({ defaultValues: initialValues });
 
+  const { mutate } = useMutation({
+    mutationFn: createAccount,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data);
+      reset();
+    },
+  });
+
   const password = watch('password');
 
-  const handleRegister = (formData: UserRegistrationForm) => {};
+  const handleRegister = (formData: UserRegistrationForm) => mutate(formData);
 
   return (
     <>
