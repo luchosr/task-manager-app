d180ca2fcec2fbd3e62beaad0ff35caf114e6882
Luciano Ramello <luchosr@gmail.com>
2024-12-05 18:11:02 +0100

    feat(types): create ForgotPasswordForm type

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -16,6 +16,8 @@ export type UserRegistrationForm = Pick<
   Auth,
   'name' | 'email' | 'password' | 'password_confirmation'
 >;
+export type ForgotPasswordForm = Pick<Auth, 'email'>;
+
 export type RequestConfirmationCodeForm = Pick<Auth, 'email'>;
 
 export type ConfirmToken = Pick<Auth, 'token'>;
