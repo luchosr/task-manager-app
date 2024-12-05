b1c94910b10c5d84253a1631e40950a6bcf78d32
Luciano Ramello <luchosr@gmail.com>
2024-12-05 13:09:19 +0100

    feat: implements confirm account mutation in Confirm Account view

--- frontend/src/views/auth/ConfirmAccountView.tsx
+++ frontend/src/views/auth/ConfirmAccountView.tsx
@@ -1,18 +1,26 @@
+import { confirmAccount } from '@/api/AuthAPI';
 import { ConfirmToken } from '@/types/index';
 import { PinInput, PinInputField } from '@chakra-ui/pin-input';
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function ConfirmAccountView() {
   const [token, setToken] = useState<ConfirmToken['token']>('');
 
+  const { mutate } = useMutation({
+    mutationFn: confirmAccount,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data);
+    },
+  });
   const handleChange = (token: ConfirmToken['token']) => {
     setToken(token);
   };
 
-  const handleComplete = (token: ConfirmToken['token']) => {
-    console.log('completado', token);
-  };
+  const handleComplete = (token: ConfirmToken['token']) => mutate({ token });
 
   return (
     <>
