203dc8b41ea8d083a7fd2632c52a3027c1fecefb
Luciano Ramello <luchosr@gmail.com>
2024-12-05 18:09:00 +0100

    feat: add email for reset password in auth emails

--- backend/src/emails/AuthEmail.ts
+++ backend/src/emails/AuthEmail.ts
@@ -20,4 +20,18 @@ export class AuthEmail {
     });
     console.log('Email sent', info.messageId);
   };
+
+  static sendPasswordResetToken = async (user: IEmail) => {
+    const info = await transporter.sendMail({
+      from: 'UpTask Manager <pC5pR@example.com>',
+      to: user.email,
+      subject: 'Reset your password',
+      text: `Please click on the following link to reset your password: `,
+      html: `<p>User: ${user.name}, please add 
+       the following token to confirm your password reset: ${user.token}</p>
+       or you can click the following link: <a href="${process.env.FRONTEND_URL}/auth/forgot-password">Reset Password</a>
+      <p>This link will expire in 10 minutes</p>`,
+    });
+    console.log('Email sent', info.messageId);
+  };
 }
