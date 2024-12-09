c05b4b92261f220a5236062ba71074b192249ca6
Luciano Ramello <luchosr@gmail.com>
2024-12-09 10:10:07 +0100

    feat(views): implement mutation in forgot password view

--- frontend/src/views/auth/ForgotPasswordView.tsx
+++ frontend/src/views/auth/ForgotPasswordView.tsx
@@ -2,6 +2,9 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { ForgotPasswordForm } from '../../types';
 import ErrorMessage from '@/components/ErrorMessage';
+import { forgotPassword } from '@/api/AuthAPI';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 
 export default function ForgotPasswordView() {
   const initialValues: ForgotPasswordForm = {
@@ -14,7 +17,17 @@ export default function ForgotPasswordView() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const handleForgotPassword = (formData: ForgotPasswordForm) => {};
+  const { mutate } = useMutation({
+    mutationFn: forgotPassword,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data);
+      reset();
+    },
+  });
+
+  const handleForgotPassword = (formData: ForgotPasswordForm) =>
+    mutate(formData);
 
   return (
     <>
