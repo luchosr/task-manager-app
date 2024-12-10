28d3c7979683a6e46243c5b3b40c04ff3c3d0040
Luciano Ramello <luchosr@gmail.com>
2024-12-10 10:21:22 +0100

    feat(api): create updatePasswordWithToken method

--- frontend/src/api/AuthAPI.ts
+++ frontend/src/api/AuthAPI.ts
@@ -1,5 +1,6 @@
 import {
   ForgotPasswordForm,
+  NewPasswordForm,
   RequestConfirmationCodeForm,
   UserLoginForm,
 } from './../types/index';
@@ -72,6 +73,24 @@ export async function forgotPassword(formData: ForgotPasswordForm) {
 export async function validateToken(formData: ConfirmToken) {
   try {
     const url = '/auth/validate-token';
+    const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function updatePasswordWithToken({
+  formData,
+  token,
+}: {
+  formData: NewPasswordForm;
+  token: ConfirmToken['token'];
+}) {
+  try {
+    const url = `/auth/update-password/${token}`;
     const { data } = await api.post<string>(url, formData);
     return data;
   } catch (error) {
