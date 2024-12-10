d6e828899744c6776a0320bafa1f70994119ec16
Luciano Ramello <luchosr@gmail.com>
2024-12-10 13:39:28 +0100

    fix(models): export IUser interface

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-interface IUser extends Document {
+export interface IUser extends Document {
   email: string;
   password: string;
   name: string;
