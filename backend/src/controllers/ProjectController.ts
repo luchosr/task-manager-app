31653a6547512658848574d02630fc2709b7d0f9
Luciano Ramello <luchosr@gmail.com>
2024-12-10 17:23:51 +0100

    fix(controllers): log user on project controller

--- backend/src/controllers/ProjectController.ts
+++ backend/src/controllers/ProjectController.ts
@@ -4,7 +4,7 @@ import Project from '../models/Project';
 export class ProjectController {
   static createProject = async (req: Request, res: Response) => {
     const project = new Project(req.body);
-
+    console.log(req.user);
     try {
       await project.save();
       res.send('Project Created');
