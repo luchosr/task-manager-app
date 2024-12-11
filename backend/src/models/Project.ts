c23a12b41e9462f6c47fab632f92060e1b1e55ae
Luciano Ramello <luchosr@gmail.com>
2024-12-11 21:22:29 +0100

    fix(models): extends project schema

--- backend/src/models/Project.ts
+++ backend/src/models/Project.ts
@@ -1,10 +1,12 @@
 import mongoose, { Schema, Document, PopulatedDoc, Types } from 'mongoose';
 import { ITask } from './Task';
+import { IUser } from './User';
 export interface IProject extends Document {
   projectName: string;
   clientName: string;
   description: string;
   tasks: PopulatedDoc<ITask & Document>[];
+  manager: PopulatedDoc<IUser & Document>;
 }
 
 const ProjectSchema: Schema = new Schema(
@@ -13,6 +15,7 @@ const ProjectSchema: Schema = new Schema(
     clientName: { type: String, required: true, trim: true },
     description: { type: String, required: true, trim: true },
     tasks: [{ type: Types.ObjectId, ref: 'Task' }],
+    manager: { type: Types.ObjectId, ref: 'User' },
   },
   { timestamps: true }
 );
