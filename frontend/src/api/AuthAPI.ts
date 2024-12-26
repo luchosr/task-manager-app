77b67b730ddc071eac493dd79406d020228abdbe
Luciano Ramello <luchosr@gmail.com>
2024-12-26 10:24:05 +0100

    feat(frontend): add get user method in auth api

--- frontend/src/api/AuthAPI.ts
+++ frontend/src/api/AuthAPI.ts
@@ -100,3 +100,15 @@ export async function updatePasswordWithToken({
     }
   }
 }
+
+export async function getUser() {
+  try {
+    const { data } = await api('/auth/user');
+    console.log(data);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
