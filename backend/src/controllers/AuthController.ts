6daf9bf95f0c59dce0832c0b0c9851ce03768a13
Luciano Ramello <luchosr@gmail.com>
2024-12-26 10:21:50 +0100

    feat(backend): add user route controller

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -206,4 +206,9 @@ export class AuthController {
       res.status(500).json({ error: 'Oops! Something went wrong' });
     }
   };
+
+  static user = async (req: Request, res: Response) => {
+    res.json(req.user);
+    return;
+  };
 }
