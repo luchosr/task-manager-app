05ef45a5226d4a8ed1dadb3e8836596e3598b057
Luciano Ramello <luchosr@gmail.com>
2024-12-26 10:24:58 +0100

    feat(frontend): implement useAuth hook in AppLayout component

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,10 +1,15 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Navigate, Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Logo from '@/components/Logo';
 import { NavMenu } from '@/components/NavMenu';
+import { useAuth } from '@/hooks/useAuth';
 
 export default function AppLayout() {
+  const { data, isError, isLoading } = useAuth();
+  if (isLoading) return <p>Cargando...</p>;
+  if (isError) return <Navigate to={'/auth/login'} />;
+
   return (
     <>
       <header className="bg-gray-800 py-5">
