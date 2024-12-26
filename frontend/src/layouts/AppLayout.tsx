58380baa4371a9c62d2af0efae4f8011c9f60258
Luciano Ramello <luchosr@gmail.com>
2024-12-26 12:59:23 +0100

    feat(frontend): implement usAuth hook in AppLayout

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -10,27 +10,28 @@ export default function AppLayout() {
   if (isLoading) return <p>Cargando...</p>;
   if (isError) return <Navigate to={'/auth/login'} />;
 
-  return (
-    <>
-      <header className="bg-gray-800 py-5">
-        <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-64">
-            <Link to="/">
-              <Logo />
-            </Link>
+  if (data)
+    return (
+      <>
+        <header className="bg-gray-800 py-5">
+          <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <div className="w-64">
+              <Link to="/">
+                <Logo />
+              </Link>
+            </div>
+            <NavMenu name={data.name} />
           </div>
-          <NavMenu />
-        </div>
-      </header>
-      <section className="max-w-screen-2xl mx-auto mt-10 p-5">
-        <Outlet />
-      </section>
-      <footer className="p-5 ">
-        <p className="text-center">
-          Todos los derechos reservados &copy; {new Date().getFullYear()}
-        </p>
-      </footer>
-      <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
-    </>
-  );
+        </header>
+        <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+          <Outlet />
+        </section>
+        <footer className="p-5 ">
+          <p className="text-center">
+            Todos los derechos reservados &copy; {new Date().getFullYear()}
+          </p>
+        </footer>
+        <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
+      </>
+    );
 }
