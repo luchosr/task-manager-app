666b939d95c5b32c9e74921814af42c1b7cc7227
Luciano Ramello <luchosr@gmail.com>
2025-01-16 18:05:59 +0100

    feat(frontend): implement UpdateCurrentUserPasswordForm type

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const authSchema = z.object({
   name: z.string(),
   email: z.string().email(),
+  current_password: z.string(),
   password: z.string(),
   password_confirmation: z.string(),
   token: z.string(),
@@ -18,6 +19,10 @@ export type UserRegistrationForm = Pick<
 >;
 export type ForgotPasswordForm = Pick<Auth, 'email'>;
 export type NewPasswordForm = Pick<Auth, 'password' | 'password_confirmation'>;
+export type UpdateCurrentUserPasswordForm = Pick<
+  Auth,
+  'current_password' | 'password' | 'password_confirmation'
+>;
 
 export type RequestConfirmationCodeForm = Pick<Auth, 'email'>;
 
@@ -35,6 +40,7 @@ export const userSchema = authSchema
   });
 
 export type User = z.infer<typeof userSchema>;
+export type UserProfileForm = Pick<User, 'name' | 'email'>;
 
 const noteSchema = z.object({
   _id: z.string(),
