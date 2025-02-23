056f664104fd751fc9584fb3919a5672a592f159
Luciano Ramello <luchosr@gmail.com>
2025-02-23 22:31:33 +0100

    feat(backend): implements route for password checking.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -97,4 +97,12 @@ router.post(
   AuthController.updateCurrentUserPassword
 );
 
+router.post(
+  '/check-password',
+  authenticate,
+  body('password').notEmpty().withMessage('Password is required'),
+  handleInputErrors,
+  AuthController.checkPassword
+);
+
 export default router;
