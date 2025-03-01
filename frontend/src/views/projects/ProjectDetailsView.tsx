1b46cc9aafa3bd6a7dce3808ac86eb89cddf227a
Luciano Ramello <luchosr@gmail.com>
2025-03-01 18:32:56 +0100

    feat(frontend): update query fn

--- frontend/src/views/projects/ProjectDetailsView.tsx
+++ frontend/src/views/projects/ProjectDetailsView.tsx
@@ -1,4 +1,4 @@
-import { getProjectById } from '@/api/ProjectAPI';
+import { getFullProject } from '@/api/ProjectAPI';
 import { useQuery } from '@tanstack/react-query';
 import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
 import AddTaskModal from '@/components/tasks/AddTaskModal';
@@ -19,7 +19,7 @@ export default function ProjectDetailsView() {
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ['project', projectId],
-    queryFn: () => getProjectById(projectId),
+    queryFn: () => getFullProject(projectId),
     retry: false,
   });
 
